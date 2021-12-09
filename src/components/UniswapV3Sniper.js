ba32c6e6046e5ff89d0d268f7dfbd78344f524a9
Paolo Rollo <paolo.rollo1997@gmail.com>
2021-12-09 10:09:35 +0100

    update.

--- src/components/UniswapV3Sniper.js
+++ src/components/UniswapV3Sniper.js
@@ -333,7 +333,7 @@ const UniswapV3Sniper = ({ web3 }) => {
                             </div>
                             <div className="mt-4 sm:mt-6">
                                 {
-                                    (!loading && !finished && parseFloat(selectedPair.reserveETH) <= 0.2) && <button
+                                    (!loading && !finished && parseFloat(selectedPair.totalValueLockedETH) <= 0.2) && <button
                                         type="button"
                                         className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:col-start-2 sm:text-sm"
                                         onClick={() => closeModal()}
@@ -342,7 +342,7 @@ const UniswapV3Sniper = ({ web3 }) => {
                                     </button>
                                 }
                                 {
-                                    (!loading && !finished && parseFloat(selectedPair.reserveETH) >= 0.2) && <button
+                                    (!loading && !finished && parseFloat(selectedPair.totalValueLockedETH) >= 0.2) && <button
                                         type="button"
                                         className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:col-start-2 sm:text-sm"
                                         onClick={() => checkPair()}
